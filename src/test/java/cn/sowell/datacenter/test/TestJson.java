5b27cad252598e8662d4160085783cc4770c1481
manxihuanchi <546568501@qq.com>
2019-07-30 16:48:39 +0800

    111

---
+++ src/test/java/cn/sowell/datacenter/test/TestJson.java
@@ -0,0 +1,30 @@
+package cn.sowell.datacenter.test;
+
+public class TestJson {
+	
+	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append(" {    \"id\": \"123\",")
+		.append("  \"name\": \"张三\",")
+		.append("  \"studentList\": [")
+		.append("  {")
+		.append("  \"id\": \"2222\",")
+		.append(" \"name\": \"李四\",")
+		.append(" \"age\": \"13\"")
+		.append(" },")
+		.append("  {")
+		.append("   \"id\": \"33333\",")
+		.append("  \"name\": \"王u\",")
+		.append(" \"age\": \"18\"")
+		.append(" }")
+		.append(" ]")
+		.append(" }");
+		
+		
+		
+		
+	}
+	
+	
+}
